6e19e070bd537e12ae7c6353fc0c7ab375e5421e
Matthew2112 <mreshetnikov669@gmail.com>
2024-02-28 14:44:55 +0300

    Zadanie 3(21/02)

--- Zadanie1(28/02/zad3.py
+++ Zadanie1(28/02/zad3.py
@@ -1,5 +1,7 @@
-s=input('Enter word')
-if (s.count('Ф')>=1 or s.count('ф')>=1 )==True:
-    print("Ого! Это редкое слово!")
-if (s.count('Ф')==0 or s.count('ф')==0)== True:
-    print("Эх, это не очень редкое слово...")
+n=int(input('Enter the number of words: '))
+for s in range (n):
+    s=input('Enter word: ')
+    if (s.count('Ф')>=1 or s.count('ф')>=1 )==True:
+        print("Ого! Это редкое слово!")
+    elif (s.count('Ф')==0 or s.count('ф')==0)== True:
+        print("Эх, это не очень редкое слово...")
